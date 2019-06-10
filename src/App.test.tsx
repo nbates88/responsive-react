6ad4ea41475f6a3a229c2c6f38c40d6ed805fa0a
Nichole Bates <nbates88@gmail.com>
2019-06-09 21:44:10 -0400

    setup cra-ts-jest-enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+  shallow(<App />);
+});
+
+it("had a p", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find("p")).toExist();
+});
+
+it("had an a", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find("a")).toHaveText("Learn React");
 });
